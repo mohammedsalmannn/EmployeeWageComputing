05dcaf73a63038989cf6b7f55b9e4d62f7a1cc1e
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-19 15:56:42 +0530

    Added: One_Day_Wage

--- src/main/java/com/bridgelabz/EmployeeWage.java
+++ src/main/java/com/bridgelabz/EmployeeWage.java
@@ -4,10 +4,14 @@ public class EmployeeWage {
     public static void main(String[] args)
     {
         final int IS_PRESENT= 1 ;
+         int PER_HR=20;
+         int FULL_DAY=8;
         double empCheck= Math.floor(Math.random()*10)%2;
         if (empCheck==IS_PRESENT)
         {
             System.out.println("employee is present");
+            int oneday_wages=(   PER_HR * FULL_DAY   );
+                    System.out.println(oneday_wages);
 
         }
         else {
