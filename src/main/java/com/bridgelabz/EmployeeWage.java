c0819c84427600a50b15d567066f335212aba165
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-19 15:22:29 +0530

    Added: chck Employe Present or Not

--- src/main/java/com/bridgelabz/EmployeeWage.java
+++ src/main/java/com/bridgelabz/EmployeeWage.java
@@ -3,6 +3,15 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static void main(String[] args)
     {
-      System.out.println("Welcome to Employee Wages");
+        final int IS_PRESENT= 1 ;
+        double empCheck= Math.floor(Math.random()*10)%2;
+        if (empCheck==IS_PRESENT)
+        {
+            System.out.println("employee is present");
+
+        }
+        else {
+            System.out.println("employee is absent");
+        }
     }
 }
