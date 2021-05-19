6447549c86424d39e77a5fda024d1efa33630a4b
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-19 20:28:37 +0530

    Added:Sitch Case

--- src/main/java/com/bridgelabz/EmployeeWage.java
+++ src/main/java/com/bridgelabz/EmployeeWage.java
@@ -3,24 +3,31 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static void main(String[] args)
     {
-        final int IS_PRESENT= 1 ;
+        //final int IS_PRESENT= 1 ;
          int PER_HR=20;
-         int FULL_DAY=8;
+        int FULL_DAY=8;
          int PART_TIME_HRs=8;
-        double empCheck= Math.floor(Math.random()*10)%2;
-        if (empCheck==IS_PRESENT)
-        {
-            System.out.println("employee is present");
-            int oneday_wages=(   PER_HR * FULL_DAY   );
-                    System.out.println("Oneday Wages is : " + oneday_wages);
-            int part= (PER_HR*PART_TIME_HRs);
-            double total= (oneday_wages + part );
-            System.out.println("PartTime Wages " + part);
-            System.out.println("TotalWages "+total);
+        //int oneday_wages=0;
+        int empCheck= (int) (Math.floor(Math.random()*10)%2);
+        switch (empCheck) {
 
-        }
-        else {
-            System.out.println("employee is absent");
+            case 0 :
+                int oneday_wages=(   PER_HR * FULL_DAY   );
+                System.out.println("Oneday Wages is : " + oneday_wages);
+                System.out.println("salman");
+                break;
+            case 1:
+                int part= (PER_HR*PART_TIME_HRs);
+                int Day= (PER_HR * FULL_DAY );
+                int total=(part+Day);
+                System.out.println("PartTime Wages " + part);
+                System.out.println("TotalWages "+total);
+                System.out.println("salman");
+                break;
+            default:
+                System.out.println("mann");
+
+
         }
 
     }
