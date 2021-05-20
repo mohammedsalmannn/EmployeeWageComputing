ce8c6123d32c98dab0f7957e1a7ecc238dd06663
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-20 07:29:39 +0530

    Added : 20day Wrking

--- src/main/java/com/bridgelabz/EmployeeWage.java
+++ src/main/java/com/bridgelabz/EmployeeWage.java
@@ -1,30 +1,40 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    public static void main(String[] args)
-    {
-        final int IS_PRESENT= 1 ;
-         int PER_HR=20;
-        int FULL_DAY=8;
-         int PART_TIME_HRs=8;
+    public static void main(String[] args) {
+        final int IS_PRESENT = 1;
+        //int Grand_Total=0;
+        int PER_HR = 20;
+        int FULL_DAY = 8;
+       // int PART_TIME_HRs = 8;
+        //int TOTAK_EMP_HR=0;
+        int MAX_WRK_HRS=100;
+        int NUM_OF_WRK_HR=0;
+        int NUM_OF_WRK_DAYS=0;
+        int MAX_WRK_DAYS=20;
+        int one_wages=0;
         //int oneday_wages=0;
-        int empCheck= (int) (Math.floor(Math.random()*10)%2);
-            if(empCheck==IS_PRESENT)
+        int total=0;
+        int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+        //int Grand_Total = 0;
+//        for (int i = 1; i <= 100; i++) {
+//            //int empCheck= (int) (Math.floor(Math.random()*10)%2);
+//
+//            Grand_Total = ((i * PER_HR ));
+        while (  NUM_OF_WRK_HR <= MAX_WRK_HRS &&  NUM_OF_WRK_DAYS <=MAX_WRK_DAYS )
+        {
+            if (empCheck == IS_PRESENT)
             {
-                int part= (PER_HR*PART_TIME_HRs);
-                int Day= (PER_HR * FULL_DAY );
-                int total=(part+Day);
-                int Gross_Total=(total*20);
-                System.out.println("PartTime Wages " + part);
-                System.out.println("TotalWages "+total);
-                System.out.println(Gross_Total);
+                one_wages = PER_HR * FULL_DAY;
+                NUM_OF_WRK_HR++;
             }
-            else{
-                int oneday_wages=(   PER_HR * FULL_DAY   );
-                   System.out.println("Oneday Wages is : " + oneday_wages);
-            }
+            else System.out.println("emp absnt");
 
+
+            NUM_OF_WRK_DAYS++;
+
+            System.out.println("Total_salary   :" + (total+=one_wages));
+        }
 
     }
-
 }
