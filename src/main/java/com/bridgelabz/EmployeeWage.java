97ccc7963a2c4eb65793cb6d6b591afd0e46731e
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-22 07:35:44 +0530

    Refactor the Code in OOPs Concept

--- src/main/java/com/bridgelabz/EmployeeWage.java
+++ src/main/java/com/bridgelabz/EmployeeWage.java
@@ -1,40 +1,53 @@
 package com.bridgelabz;
 
+import java.lang.*;
+
 public class EmployeeWage {
+    public static int empCheck;
+    public static int PER_HR = 20;
+    public static int FULL_DAY = 8;
+    public static int PART_TIME_HRs = 4;
+    public static int one_wages = 0;
+    public static int total=0;
+
+
     public static void main(String[] args) {
         final int IS_PRESENT = 1;
-        //int Grand_Total=0;
-        int PER_HR = 20;
-        int FULL_DAY = 8;
-       // int PART_TIME_HRs = 8;
-        //int TOTAK_EMP_HR=0;
-        int MAX_WRK_HRS=100;
-        int NUM_OF_WRK_HR=0;
-        int NUM_OF_WRK_DAYS=0;
-        int MAX_WRK_DAYS=20;
-        int one_wages=0;
-        //int oneday_wages=0;
-        int total=0;
-        int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
-        //int Grand_Total = 0;
-//        for (int i = 1; i <= 100; i++) {
-//            //int empCheck= (int) (Math.floor(Math.random()*10)%2);
-//
-//            Grand_Total = ((i * PER_HR ));
-        while (  NUM_OF_WRK_HR <= MAX_WRK_HRS &&  NUM_OF_WRK_DAYS <=MAX_WRK_DAYS )
-        {
-            if (empCheck == IS_PRESENT)
-            {
-                one_wages = PER_HR * FULL_DAY;
-                NUM_OF_WRK_HR++;
-            }
-            else System.out.println("emp absnt");
+        int MAX_WRK_HRS = 100;
+        int NUM_OF_WRK_HR = 0;
+        int NUM_OF_WRK_DAYS = 0;
+        int MAX_WRK_DAYS = 20;
+        int total = 0;
 
 
+        while (NUM_OF_WRK_HR <= MAX_WRK_HRS && NUM_OF_WRK_DAYS <= MAX_WRK_DAYS) {
+            int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+            int salary = Calculate(empCheck);
             NUM_OF_WRK_DAYS++;
-
-            System.out.println("Total_salary   :" + (total+=one_wages));
+            System.out.println("Total_salary   :" + (total += one_wages));
         }
 
+    }
+
+
+
+
+    private static int Calculate(int empCheck) {
+        switch (empCheck) {
+            case 1:
+                one_wages = PER_HR * FULL_DAY;
+
+                break;
+            case 2:
+                one_wages = PART_TIME_HRs * PER_HR;
+                break;
+            default:
+                System.out.println("emp absnt");
+                break;
+
+        }
+
+
+        return total;
     }
 }
