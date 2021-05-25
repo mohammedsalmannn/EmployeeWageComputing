df628e081598847e29fb701cfdfbc79366bed1a1
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-25 21:30:24 +0530

    Added: Factinality to Save Total Wage of the Company

--- src/main/java/com/bridgelabz/EmployeeWage.java
+++ src/main/java/com/bridgelabz/EmployeeWage.java
@@ -1,16 +1,18 @@
 package com.bridgelabz;
 
+import jdk.nashorn.internal.runtime.NumberToString;
+
 import java.lang.*;
 
-public class EmployeeWage<empHrs> {
+public class EmployeeWage {
     public static int empCheck;
     private static int empHrs = 0;
-    public int PER_HR;
+    public static int PER_HR;
     public int FULL_DAY;
     public int PART_TIME_HRs;
     public static int one_wages = 0;
     //public static int total=0;
-    private String company;
+    private static String company;
     public static int NUM_OF_WRK_HR;
     public static int NUM_OF_WRK_DAYS;
     public static int MAX_WRK_DAYS;
@@ -21,7 +23,7 @@ public class EmployeeWage<empHrs> {
     public static int total = 0;
 
 
-    public static int computeWgae(String company, int PER_HR, int NUM_OF_WRK_DAYS, int MAX_WRK_DAYS) {
+    public static int computeWgae() {
         while (totalEmpHr <= MAX_WRK_DAYS && totalDAYS < NUM_OF_WRK_DAYS) {
             int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
             switch (empCheck) {
@@ -50,16 +52,30 @@ public class EmployeeWage<empHrs> {
     }
 
 
-//    public EmployeeWage( String company,int PER_HR, int NUM_OF_WRK_DAYS, int MAX_WRK_DAYS) {
-//        this.PER_HR = PER_HR;
-//        this.company = company;
-//        this.NUM_OF_WRK_DAYS = NUM_OF_WRK_DAYS;
-//        this.MAX_WRK_DAYS = MAX_WRK_DAYS;
-//    }
+    public EmployeeWage( String company,int PER_HR, int NUM_OF_WRK_DAYS, int MAX_WRK_DAYS) {
+        this.PER_HR = PER_HR;
+        this.company = company;
+        this.NUM_OF_WRK_DAYS = NUM_OF_WRK_DAYS;
+        this.MAX_WRK_DAYS = MAX_WRK_DAYS;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeWage{" +
+                "PER_HR=" + PER_HR +
+                ", FULL_DAY=" + FULL_DAY +
+                ", PART_TIME_HRs=" + PART_TIME_HRs +
+                ", company='" + company + '\'' +
+                '}';
+    }
 
     public static void main(String[] args) {
-        computeWgae("Dmart",20,2,10);
-        computeWgae("salman",10,4,15);
+        EmployeeWage dmart = new EmployeeWage("Dmart",20,2,10);
+        EmployeeWage relience = new EmployeeWage("relience",25,4,15);
+        dmart.computeWgae();
+        System.out.println(dmart);
+        relience.computeWgae();
+        System.out.println(relience);
 
 
 
